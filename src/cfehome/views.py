65bb598a91e2279362e57a7e65f6275f63f2bde9
dabid <puchungualotsqui@gmail.com>
2024-08-11 00:06:09 +1000

    Hello World msg

---
+++ src/cfehome/views.py
@@ -0,0 +1,34 @@
+import pathlib
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from visits.models import PageVisit
+
+this_dir = pathlib.Path(__file__).resolve().parent
+def home_page_view(request, *args, **kwargs):
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+    my_title = 'My page'
+    my_context = {
+        'page_title': my_title,
+        'page_visit_count': page_qs.count(),
+        'total_visit_count': qs.count()
+    }
+    path = request.path
+    print(path)
+    html_template = 'home.html'
+    PageVisit.objects.create(path=request.path)
+    return render(request, html_template, my_context)
+
+
+
+def old_home_page_view(request, *args, **kwargs):
+    my_title = 'My page'
+    my_content = {
+        'page_title': my_title
+    }
+    html = ''
+    html_file_path = this_dir / 'home.html'
+    html = html_file_path.read_text()
+    html = html.format(**my_content)
+    return HttpResponse(html)
