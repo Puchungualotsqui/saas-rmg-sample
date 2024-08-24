77056c1fbc9101a4811fb1fbebd093623adca236
dabid <puchungualotsqui@gmail.com>
2024-08-24 22:26:43 +1000

    Prepare for railway

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -25,8 +25,11 @@ SECRET_KEY = 'django-insecure-!ti^2ly8t068195s=^@d@jhz!q1ll(7wycwy5om5(hh0m9h#=8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    '.railway.app' # https://saas.prod.railway.app
+]
+if DEBUG:
+    ALLOWED_HOSTS += ['127.0.0.1', 'localhost']
 
 # Application definition
 
--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -5,14 +5,22 @@ from django.http import HttpResponse
 from visits.models import PageVisit
 
 this_dir = pathlib.Path(__file__).resolve().parent
-def home_page_view(request, *args, **kwargs):
+def home_view(request, *args, **kwargs):
+    return about_view(request, *args, **kwargs)
+
+def about_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path=request.path)
+    try:
+        percent = (page_qs.count()*100)/qs.count()
+    except:
+        percent = 0
     my_title = 'My page'
     html_template = 'home.html'
     my_context = {
         'page_title': my_title,
         'page_visit_count': page_qs.count(),
+        'percent': percent,
         'total_visit_count': qs.count()
     }
     PageVisit.objects.create(path=request.path)
